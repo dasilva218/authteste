c9d41fee762df2c4dbef83b07aaac1aecfcb9cec
dasilva218 <dasilvawil218@gmail.com>
2024-01-10 09:43:09 +0100

    gestion des models

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { router } from './router/index.js';
-import { teste } from './router/teste.js';
+
 
 const app = express();
 const server = createServer(app);
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:true}))
 
 
 app.use('', router);
-app.use('', teste)
+
 
 server.listen(8080, () => {
   console.log(`serveur lancé`);
